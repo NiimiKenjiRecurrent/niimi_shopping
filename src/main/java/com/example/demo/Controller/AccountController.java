44e1d981ec9e06ee5809956fcffd8635afd6975a
Kenji Niimi <niimi.kenji.recurrent@gmail.com>
2022-05-19 14:29:54 +0900

    login表面作成

--- src/main/java/com/example/demo/Controller/AccountController.java
+++ src/main/java/com/example/demo/Controller/AccountController.java
@@ -1,13 +1,52 @@
 package com.example.demo.Controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class AccountController {
+	@Autowired
+	HttpSession session;
+	
+//	@Autowired
+//	AccountRepository accountRepository;
+
+	
 	@RequestMapping("/")
 	public String login() {
 		return "account/index";
 	}
-	// account/index
+	
+	@RequestMapping(value="/login",method=RequestMethod.POST)
+	public ModelAndView login(
+			ModelAndView mv,
+			@RequestParam("email") String email,
+			@RequestParam("password") String password) {
+		
+		//未入力チェック
+		if(email==null||email.length()==0) {
+			mv.addObject("message","未入力");
+			mv.setViewName("account/index");
+		}else {
+			// ok
+			session.setAttribute("name", email);
+			mv.setViewName("item/showItem");
+		}
+		return mv;
+	}
+	
+	@RequestMapping("signup")
+	public ModelAndView signup(ModelAndView mv) {
+		
+		mv.setViewName("account/signUp");
+		return mv;
+	}
+	
 }
